ef8cb56b8b32a45b607c73af96ee6b530dcd6041
David Lee <Davideuijinlee@gmail.com>
2020-08-16 16:51:00 -0700

    drag and drop library works currently 1 column

---
+++ drag-practice/src/initial-data.js
@@ -0,0 +1,19 @@
+const initialData = {
+    tasks: {
+        'task-1': { id: 'task-1', content: 'stuff'}, 
+        'task-2': { id: 'task-2', content: 'more stuff'}, 
+        'task-3': { id: 'task-3', content: 'more more stuff'}, 
+        'task-4': { id: 'task-4', content: 'more more more stuff'}, 
+        'task-5': { id: 'task-5', content: 'more more more more stuff'}, 
+    },
+    columns: {
+        'column-1': {
+            id: 'column-1',
+            title: 'To Do',
+            taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+        },
+    },
+    columnOrder: ['column-1'],
+}
+
+export default initialData;
